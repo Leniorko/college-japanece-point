bf6e2ae196d3f47e5f1bee3c3df0614932ba740b
Ivan Egorov <www.Len244@gmail.com>
2021-05-17 02:49:11 +0300

    I want to sleep it is 3am at my place already.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,3 +1,15 @@
 export default function GameCardComponent() {
-  return <div></div>;
+  return (
+    <div className="game-card">
+      <div className="img-container">
+        <img src={process.env.PUBLIC_URL + "/placeholder_200x250.png"} alt="" />
+      </div>
+      <div>Game Name</div>
+      <div className="image-description">Some description</div>
+      <div className="price-container">
+        <div className="old-price"></div>
+        <div className="new-price"></div>
+      </div>
+    </div>
+  );
 }
