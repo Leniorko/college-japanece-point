eb3c0aa9f2d498e74fe17a153e12d01086522924
Ivan Egorov <www.Len244@gmail.com>
2021-05-31 00:36:06 +0300

    Fixing footer

--- src/components/Footers/Footer.tsx
+++ src/components/Footers/Footer.tsx
@@ -1,7 +1,10 @@
+import { useLocation } from "react-router";
 import "./Footer.css";
 
 export default function FooterComponent() {
-  return (
+  let location = useLocation();
+
+  return location.pathname === "/" || location.pathname === "" ? (
     <div className="footer">
       <div className="footer__form-container">
         <h3 className="footer__heading">Подпишись на рассылку</h3>
@@ -13,6 +16,19 @@ export default function FooterComponent() {
         />
         <button className="footer__send-btn">Send</button>
       </div>
+      <div className="footer__about-container">
+        <img
+          src={process.env.PUBLIC_URL + "/resources/Logo.svg"}
+          alt="Japanese Point"
+          className="footer__logo"
+        />
+        <p className="footer__about-text">
+          © 2021, Egorov Ivan, Vernidub Artem
+        </p>
+      </div>
+    </div>
+  ) : (
+    <div className="footer">
       <div className="footer__about-container">
         <img
           src={process.env.PUBLIC_URL + "/resources/Logo.svg"}
